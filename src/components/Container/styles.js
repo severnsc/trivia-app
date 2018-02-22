d3344c286fe25c60a070000a5fe02732679928bb
severnsc <severnsc@gmail.com>
2018-02-22 12:29:36 -0500

    Change justifyContent for container

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -5,6 +5,6 @@ export default styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
