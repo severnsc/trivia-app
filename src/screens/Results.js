25ba7a5d48bc2aac2d8f733838e53f63842e6735
severnsc <severnsc@gmail.com>
2018-02-21 22:57:51 -0500

    Add Results Screen

--- src/screens/Results.js
+++ src/screens/Results.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Text, Button } from 'react-native'
+import { Text, Button, FlatList } from 'react-native'
 import Container from '../components/Container'
+
+const data = [
+  {id: 1, text: "Unturned originally started as a Roblox game."},
+  {id: 2, text: "You are a duck."}
+]
 
 export default Results = () => (
   <Container>
     <Text>Your Scored</Text>
     <Text>3/10</Text>
+    <FlatList
+      data={data}
+      renderItem={({item}) => <Text>{item.text}</Text>}
+      keyExtractor={(item, index) => item.id}
+    />
     <Button onPress={() => {}} title="PLAY AGAIN?" />
   </Container>
 )
