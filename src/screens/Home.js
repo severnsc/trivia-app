4959f2be65117d0078a7427059b7ed3ebf9f9ec4
severnsc <severnsc@gmail.com>
2018-02-23 11:27:58 -0500

    Change home screen text

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text, Button } from 'react-native';
 import Container from '../components/common/Container'
+import { H1 } from '../components/common/Typography'
 
 export default Home = ({navigation}) => (
   <Container>
-    <Text>Welcome to the Trivia Challenge!</Text>
-    <Text>Your will be presented with 10 True or False questions.</Text>
-    <Text>Can you score 100%</Text>
+    <H1>Welcome to the Trivia Challenge!</H1>
+    <Text>You will be presented with 10 True or False questions.</Text>
+    <Text>Can you score 100%?</Text>
     <Button title="BEGIN" onPress={() => navigation.navigate('Quiz', {name: "Quiz"})} />
   </Container>
 )
