63d1e3cc5efc66afea636fe843de2e786a6374eb
severnsc <severnsc@gmail.com>
2018-02-24 15:10:03 -0500

    Refactor mutations

--- src/graphql/completeQuiz.js
+++ src/graphql/completeQuiz.js
@@ -1,15 +1,5 @@
 import gql from 'graphql-tag'
 
-const AnsweredQuestion = gql`
-  input AnsweredQuestion {
-    category: String!
-    questionText: String!
-    correctAnswer: String!
-    userAnswer: String!
-    correct: Boolean!
-  }
-`
-
 export default gql`
   mutation answerQuestion($answeredQuestion: AnsweredQuestion!) {
 
